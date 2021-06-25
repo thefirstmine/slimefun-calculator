37d5567656b46e9ae9855d496dfd01e952aa23b1
lawrencetheabhorrence <luluemilianolawrence@gmail.com>
2021-06-25 18:05:07 +0800

    fix

--- materials.js
+++ materials.js
@@ -25,7 +25,7 @@ const materials = {
     .setTitle(info.title)
     .setThumbnail(info.thumbnail)
     .setColor("BLUE")
-    .addFields(...info.items.map(a => a.value = value * amount))
+    .addFields(...info.items.map(({name, value}) => {name, value: value * amt }))
   
     channel.send(embed)
   }
